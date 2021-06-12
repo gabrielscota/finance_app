444ac68d831685cf423bb1843066f6398c31c7b1
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 20:43:38 -0300

    feat: user authentication with google usecase

---
+++ lib/domain/usecases/user_google_signin.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+import '../entities/entities.dart';
+
+abstract class UserGoogleSignIn {
+  Future<String?> authWithGoogle({required GoogleSignUpParams params});
+}
+
+class GoogleSignUpParams extends Equatable {
+  final UserEntity user;
+
+  const GoogleSignUpParams({required this.user});
+
+  @override
+  List<Object> get props => [user];
+}
