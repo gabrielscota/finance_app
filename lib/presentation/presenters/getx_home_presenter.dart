2ea22ba08328caf1241467d0138e4f557f59f0aa
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 16:41:06 -0300

    feat: added navigation manager

--- lib/presentation/presenters/getx_home_presenter.dart
+++ lib/presentation/presenters/getx_home_presenter.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 
 import '../../ui/pages/pages.dart';
+import '../mixins/mixins.dart';
 
-class GetxHomePresenter extends GetxController implements HomePresenter {}
+class GetxHomePresenter extends GetxController with NavigationManager implements HomePresenter {}
