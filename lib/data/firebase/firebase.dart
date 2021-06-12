1271b1bf142912a20d792db0ebe9ed629c743a7d
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 20:49:40 -0300

    feat: firebase firestore usecase and handle error

--- lib/data/firebase/firebase.dart
+++ lib/data/firebase/firebase.dart
@@ -1,2 +1,4 @@
 export './firebase_auth.dart';
 export './firebase_auth_error.dart';
+export './firebase_firestore.dart';
+export './firebase_firestore_error.dart';
---
+++ lib/data/firebase/firebase_firestore.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class CloudFirestore {
+  CollectionReference getCollection({
+    required String collectionName,
+  });
+
+  Stream<QuerySnapshot> getStreamCollection({
+    required String collectionName,
+  });
+}
