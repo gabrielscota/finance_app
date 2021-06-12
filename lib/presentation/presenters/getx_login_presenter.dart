c4ca0d03f56485bad962a3e045cba4b5161c478f
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 16:41:22 -0300

    feat: added navigation manager and method to go to home page

--- lib/presentation/presenters/getx_login_presenter.dart
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -1,5 +1,11 @@
 import 'package:get/get.dart';
 
 import '../../ui/pages/pages.dart';
+import '../mixins/mixins.dart';
 
-class GetxLoginPresenter extends GetxController implements LoginPresenter {}
+class GetxLoginPresenter extends GetxController with NavigationManager implements LoginPresenter {
+  @override
+  void goToHomePage() {
+    navigateToWithArgs = const NavigationArguments(route: '/home');
+  }
+}
