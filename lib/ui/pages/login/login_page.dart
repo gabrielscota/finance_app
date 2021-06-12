45db3a591543a4ea3b04d5a7c3fd964e5e9ce77c
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 16:43:23 -0300

    feat: added ui navigation mixin handler

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:local_auth/auth_strings.dart';
 import 'package:local_auth/local_auth.dart';
 
+import '../../mixins/mixins.dart';
 import '../pages.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
 
   const LoginPage({Key? key, required this.presenter}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with NavigationManager {
+  @override
+  void initState() {
+    handleNavigationWithArgs(widget.presenter.navigateToWithArgsStream);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +54,9 @@ class LoginPage extends StatelessWidget {
                 );
               }
 
-              debugPrint('Authentication with biometric: ${isAuthenticated.toString()}');
+              if (isAuthenticated) {
+                widget.presenter.goToHomePage();
+              }
             },
             child: const Text('Entrar'),
           ),
