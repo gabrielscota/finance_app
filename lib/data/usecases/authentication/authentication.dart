02f26e075374d619d01c0d235692487785a63f1a
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 22:30:03 -0300

    fix: export authentication data usecases

---
+++ lib/data/usecases/authentication/authentication.dart
@@ -0,0 +1,4 @@
+export './remote_authentication.dart';
+export './remote_google_signin.dart';
+export './remote_logout.dart';
+export './remote_signup.dart';
--- lib/data/usecases/usecases.dart
+++ lib/data/usecases/usecases.dart
@@ -1,3 +1,4 @@
+export './authentication/authentication.dart';
 export './delete_current_user/delete_current_user.dart';
 export './load_user/load_user.dart';
 export './save_current_user/save_current_user.dart';
