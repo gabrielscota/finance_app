e4482843f164f3d3d83a8466d2b56488403667ac
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 21:07:55 -0300

    feat: local save current user usecase

---
+++ lib/data/usecases/save_current_user/local_save_current_user.dart
@@ -0,0 +1,18 @@
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../cache/cache.dart';
+
+class LocalSaveCurrentUser implements SaveCurrentUser {
+  final SaveSecureCacheStorage saveSecureCacheStorage;
+
+  LocalSaveCurrentUser({required this.saveSecureCacheStorage});
+
+  @override
+  Future<void> save({required String userUID}) async {
+    try {
+      await saveSecureCacheStorage.save(key: 'uid', value: userUID);
+    } catch (error) {
+      throw DomainError.unexpected;
+    }
+  }
+}
---
+++ lib/data/usecases/save_current_user/save_current_user.dart
@@ -0,0 +1,1 @@
+export './local_save_current_user.dart';
