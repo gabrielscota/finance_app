4df28012b02054cc1d988d660feb33c73dd5aedc
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-12 21:18:39 -0300

    fix: remove unused method

--- lib/data/firebase/firebase_auth.dart
+++ lib/data/firebase/firebase_auth.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 abstract class FirebaseAuthentication {
   Future<UserCredential> authWithEmailAndPassword({required String email, required String password});
   Future<UserCredential> signInWithGoogle();
-  Future<UserCredential> signInWithFacebook();
   Future<UserCredential> signUpWithEmailAndPassword({required String email, required String password});
   Future<void> logout();
 }
