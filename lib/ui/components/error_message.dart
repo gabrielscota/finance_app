85c49c00da0a73ad57b36e993e8f1af6318c5c57
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-06-17 01:54:26 -0300

    feat: change system navigation bar color to better ui

--- lib/ui/components/error_message.dart
+++ lib/ui/components/error_message.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void showErrorMessage(BuildContext context, String error) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    backgroundColor: Theme.of(context).colorScheme.error,
-    content: Text(
-      error,
-      textAlign: TextAlign.center,
-      style: Theme.of(context).textTheme.subtitle2?.copyWith(
-            color: Theme.of(context).colorScheme.onError,
-          ),
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Theme.of(context).colorScheme.error,
     ),
-  ));
+  );
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: Theme.of(context).colorScheme.error,
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      content: Text(
+        error,
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.subtitle1?.copyWith(
+              color: Theme.of(context).colorScheme.onError,
+            ),
+      ),
+    ),
+  );
+  Future.delayed(const Duration(milliseconds: 4500), () {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: Theme.of(context).colorScheme.background,
+      ),
+    );
+  });
 }
